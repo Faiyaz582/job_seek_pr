068ad616dce23f2fe839e5e57c1e3ef7dcb90f76
zafrins <zafrin1412@gmail.com>
2025-03-08 01:27:41 +0600

    Edit user

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -15,4 +16,44 @@ class UserController extends Controller
             'users' => $users
         ]);
     }
+
+    public function edit($id){
+        $user = User::findOrFail($id);
+        return view ('admin.users.edit',[
+            'user' => $user
+        ]);
+    }
+
+    public function update ($id, Request $request){
+        $validator=Validator::make($request->all(),[
+            'name'=>'required|min:5|max:20',
+            'email'=>'required|email|unique:users,email,'.$id.',id',
+           ]);
+   
+           if($validator->passes()){
+               //for updating the data
+               $user=User::find($id);
+               $user->name=$request->name;
+               $user->email=$request->email;
+               $user->mobile=$request->mobile;
+               $user->designation=$request->designation;
+               $user->save();
+             
+   
+               session()->flash('success','User info updated successfully');
+               return response()->json([
+                   'status'=>true,
+                   'errors'=>[],
+               ]);
+           
+           
+           
+           }else{
+               return response()->json([
+                   'status'=>false,
+                   'errors'=>$validator->errors(),
+               ]);
+           }
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,10 @@ Route::post('/save-job', [JobsController::class, 'saveJob'])->name('saveJob');
 Route::group(['prefix'=>'admin','middleware'=>'checkRole'],function(){
   Route::get('/dashboard',[DashboardController::class,'index'])->name('admin.dashboard');
   Route::get('/users',[UserController::class,'index'])->name('admin.users');
+  Route::get('/users/{id}',[UserController::class,'edit'])->name('admin.users.edit');
+  Route::put('/users/{id}',[UserController::class,'update'])->name('admin.users.update');
+
+
 
 });
 
@@ -45,7 +49,8 @@ Route::group(['prefix'=>'account'],function(){
           Route::post('/account/process-register',[AccountController::class,'processRegistration'])->name('front.account.processRegistration');
           Route::post('/account/authenticate',[AccountController::class,'authenticate'])->name('front.account.authenticate');
           Route::get('/account/login',[AccountController::class,'login'])->name('front.account.login');
-      });
+
+        });
 
       //Authenticated Routes
       Route::group(['middleware'=>'auth'],function(){
